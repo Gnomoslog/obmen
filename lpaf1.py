86360a25921132f391fcf661d4290f2ef945b93d
GnomosloG <romaderiie@gmail.com>
2026-04-02 11:17:42 +0300

    Add files via upload

--- lpaf1.py
+++ lpaf1.py
@@ -1,36 +1,44 @@
-# задание1 
-# n = int(input("Ведение знач"))
-# a0, a1, = 0, 1
-# for i in range(n):
-#     a_ne = a0 + a1
-#     a0 = a1
-#     a1 = a_ne
-# print(a_ne)
+# задание 1
+# a = input("Введите числа через пробел: ")
+# b = input("Введите числа через пробел: ")
+# arr = list(map(int, a.split()))
+# arr = list(map(int, b.split()))
+# a_1 = set(a) - set(b)
+# a_2 = set(b) - set(a)
+
+# print(a_1)
+# print(a_2)
+
+
 # задание 2
-# n = int(input("Ведение знач"))
-# m = [int(i) for i in input("Input m").split()[:n]]
-# print([min(m) if i == max(m) else i for i in m])
+
+# n = int(input("Введите кол-во элементов списка: "))
+# first_list = [int(i) for i in input("Введите значения списка: ").split()[:n]]
+
+# print(sum([first_list[i - 1] < first_list[i] > first_list[i + 1]
+#            for i in range(1, n - 1)]))
 
 
 # задание 3
 
-# def mam_b(n):
-#     for i in range(2, int(n ** 0.5) + 1):
-#         if n % i == 0:
-#             return False
-#     return (n != 1) * True
+# numbers = [int(i) for i in input("Введите числа: ").split()]
+# count_numbers = {}
+# for i in numbers:
+#     if i not in count_numbers:
+#         count_numbers[i] = 1    
+#     else:
+#         count_numbers[i] += 1
+# print(sum([i // 2 for i in count_numbers.values()]))
 
 
-# print("Yes" if mam_b(int(input("faffsa"))) else "no")
+# PDF ЗАДАНИЕ 4
 
-
-# задание 4 
-
-# def re(n):
-#     if n == 0:
-#         return ""
-#     x = int(input("faffsa"))
-#     return re(n - 1) + f" {x}"
-
-# n = int(input("faffsa"))
-# print(re(n))
+# a = int(input("ведите"))
+# data = {}
+# for i in range(2, a + 1):
+#     s = 1
+#     for g in range(2, i // 2 + 1):
+#         if i % g == 0:
+#             s += g
+#     data[i] = s
+# print(data)
